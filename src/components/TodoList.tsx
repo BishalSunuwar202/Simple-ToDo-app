8b58b68c3f7e516da5a5542ca1f6eb3faca29777
Bishal Sunuwar <bishalsunuwar24@gmail.com>
2023-09-04 22:25:02 +0545

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./styles.css";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
+  console.log(todos);
+  return (
+    // <div className="todos">
+    //   {todos.map((todo) => (
+    //     <SingleTodo
+    //       todo={todo}
+    //       key={todo.id }
+    //       todos={todos}
+    //       setTodos={setTodos}
+    //     />
+    //   ))}
+    // </div>
+    <div className="container">
+      <div className="todos">
+        <span className="todos_heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+
+      <div className="remove">
+      <span className="todos_heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
